7f38eb5551e3903fd940b1f14013198c720fcaaa
Cesar Mauri <cesar@crea>
2015-01-22 18:23:09 +0100

    add native code (jni) basic support

---
+++ eviacam_service/jni/Android.mk
@@ -0,0 +1,14 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+include $(OPENCV_ANDROID_SDK)/native/jni/OpenCV.mk
+
+LOCAL_MODULE    := visionpipeline
+LOCAL_SRC_FILES := visionpipeline.cpp
+LOCAL_LDLIBS    += -lm -llog -landroid
+LOCAL_STATIC_LIBRARIES += android_native_app_glue
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,android/native_app_glue)

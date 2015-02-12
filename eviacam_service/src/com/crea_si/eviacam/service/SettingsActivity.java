31a0c57f4a7734a3bd36af365d21e62d38682088
Cesar Mauri <cesar@crea>
2015-02-12 19:20:27 +0100

    bare bones settings activity with fragments
    based on:
    http://stackoverflow.com/questions/23523806/how-do-you-create-preference-activity-and-preference-fragment-on-android
    http://developer.android.com/guide/topics/ui/settings.html#Fragment

--- eviacam_service/src/com/crea_si/eviacam/service/SettingsActivity.java
+++ eviacam_service/src/com/crea_si/eviacam/service/SettingsActivity.java
@@ -1,16 +1,28 @@
 package com.crea_si.eviacam.service;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
 
-public class SettingsActivity extends PreferenceActivity {
-
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+public class SettingsActivity extends Activity {
     
-        EVIACAM.debug("SettingsActivity.onCreate()");
-    }
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
 
+            // Load the preferences from an XML resource
+            addPreferencesFromResource(R.xml.preference_fragment);
+        }
+    }
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Display the fragment as the main content.
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+    }
 }

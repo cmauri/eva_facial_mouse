c7072d08b9089c2285e19cd722fccce5bcdb6c59
Cesar Mauri <cesar@crea>
2015-03-26 12:41:59 +0100

    refactor accessibility node debugging code

--- eviacam_service/src/com/crea_si/eviacam/service/AccessibilityAction.java
+++ eviacam_service/src/com/crea_si/eviacam/service/AccessibilityAction.java
@@ -51,7 +51,8 @@ class AccessibilityAction {
             
             if (node == null) return;
             
-            EVIACAM.debug("Actionable node found: (" + p.x + ", " + p.y + ")." + getNodeInfo(node));
+            EVIACAM.debug("Actionable node found: (" + p.x + ", " + p.y + ")." + 
+                    AccessibilityNodeDebug.getNodeInfo(node));
             
             int availableActions= FULL_ACTION_MASK & node.getActions();
             
@@ -122,123 +123,5 @@ class AccessibilityAction {
         }
 
         return result;
-    }
-
-    
-    /*
-     * DEBUGGING CODE
-     * 
-     *  Code below is only intended for debugging
-     */
-    static private String getNodeInfo (AccessibilityNodeInfo node) {
-        String result= "[";
-        
-        // actions
-        if (node.isClickable()) result+= "CL.";
-        else result+= "...";
-        
-        if (node.isLongClickable()) result+= "LC.";
-        else result+= "...";
-        
-        if (node.isCheckable()) result+= "CH.";
-        else result+= "...";
-        
-        if (node.isFocusable()) result+= "FO.";
-        else result+= "...";
-        
-        if (node.isScrollable()) result+= "SC.";
-        else result+= "...";
-        
-        if (node.isVisibleToUser()) result+= "VI]";
-        else result+= "..]";
-        
-        result+= "; ";
-        
-        result+= node.getClassName();
-        
-        result+= "; ";
-
-        result+= node.getText(); 
-        
-        result+= "; ";
-        
-        result+= node.getContentDescription();
-                        
-        result+= "; ";
-       
-        // use getActions instead of getActionList because the latter crashes 
-        int actions= node.getActions();
-        
-        result+= "; [";
-        
-        if ((actions & AccessibilityNodeInfo.ACTION_FOCUS) != 0) {
-            result+= "ACTION_FOCUS, ";
-        }
-        if ((actions & AccessibilityNodeInfo.ACTION_CLEAR_FOCUS) != 0) {
-            result+= "ACTION_CLEAR_FOCUS, ";
-        }
-        if ((actions & AccessibilityNodeInfo.ACTION_SELECT) != 0) {
-            result+= "ACTION_SELECT, ";
-        }
-        if ((actions & AccessibilityNodeInfo.ACTION_CLEAR_SELECTION) != 0) {
-            result+= "ACTION_CLEAR_SELECTION, ";
-        }
-        if ((actions & AccessibilityNodeInfo.ACTION_ACCESSIBILITY_FOCUS) != 0) {
-            result+= "ACTION_ACCESSIBILITY_FOCUS, ";
-        }
-        if ((actions & AccessibilityNodeInfo.ACTION_CLEAR_ACCESSIBILITY_FOCUS) != 0) {
-            result+= "ACTION_CLEAR_ACCESSIBILITY_FOCUS, ";
-        }
-        if ((actions & AccessibilityNodeInfo.ACTION_LONG_CLICK) != 0) {
-            result+= "ACTION_LONG_CLICK, ";
-        }
-        if ((actions & AccessibilityNodeInfo.ACTION_NEXT_AT_MOVEMENT_GRANULARITY) != 0) {
-            result+= "ACTION_NEXT_AT_MOVEMENT_GRANULARITY, ";
-        }
-        if ((actions & AccessibilityNodeInfo.ACTION_PREVIOUS_AT_MOVEMENT_GRANULARITY) != 0) {
-            result+= "ACTION_PREVIOUS_AT_MOVEMENT_GRANULARITY, ";
-        }
-        if ((actions & AccessibilityNodeInfo.ACTION_NEXT_HTML_ELEMENT) != 0) {
-            result+= "ACTION_NEXT_HTML_ELEMENT, ";
-        }
-        if ((actions & AccessibilityNodeInfo.ACTION_PREVIOUS_HTML_ELEMENT) != 0) {
-            result+= "ACTION_PREVIOUS_HTML_ELEMENT, ";
-        }
-        if ((actions & AccessibilityNodeInfo.ACTION_SCROLL_FORWARD) != 0) {
-            result+= "ACTION_SCROLL_FORWARD, ";
-        }
-        if ((actions & AccessibilityNodeInfo.ACTION_SCROLL_BACKWARD) != 0) {
-            result+= "ACTION_SCROLL_BACKWARD, ";
-        }
-    
-        result+= "]";
-
-        //node.isPassword();
-        
-        /*
-        Rect boundsInParent = new Rect();
-        node.getBoundsInParent(boundsInParent);
-        
-        Rect boundsInScreen = new Rect();
-        node.getBoundsInScreen(boundsInScreen);
-        */
- 
-        return result;
-    }
-    
-    static private void displayFullTree (AccessibilityNodeInfo node) {
-        EVIACAM.debug("Accesibility tree dump:");
-        
-        displayFullTree0(node, "1");
-    }
-    
-    static private void displayFullTree0 (AccessibilityNodeInfo node, String prefix) {
-        EVIACAM.debug(prefix + " " + getNodeInfo(node));
-        
-        // propagate calls to children
-        for (int i= 0; i< node.getChildCount(); i++) {
-            String newPrefix= " " + prefix + "." + Integer.toString(i + 1);
-            displayFullTree0(node.getChild(i), newPrefix);
-        }
     }
 }

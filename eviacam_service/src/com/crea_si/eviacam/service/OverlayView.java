fec3b0442d7bf279291c9f4c05aee7c344d64809
Cesar Mauri <cesar@crea>
2015-03-30 17:39:29 +0200

    review overlay type and flags and improve comments

--- eviacam_service/src/com/crea_si/eviacam/service/OverlayView.java
+++ eviacam_service/src/com/crea_si/eviacam/service/OverlayView.java
@@ -18,12 +18,35 @@ public class OverlayView extends RelativeLayout {
         // Transparent background
         feedbackParams.format = PixelFormat.TRANSLUCENT;    
         
-        // Create an always on top window
+        /*
+         * Type of window- Create an always on top window
+         * 
+         * TYPE_PHONE: These are non-application windows providing user interaction with the
+         *      phone (in particular incoming calls). These windows are normally placed above 
+         *      all applications, but behind the status bar. In multiuser systems shows on all 
+         *      users' windows.
+         *      
+         * TYPE_SYSTEM_OVERLAY: system overlay windows, which need to be displayed on top of 
+         *      everything else. These windows must not take input focus, or they will interfere 
+         *      with the keyguard. In multiuser systems shows only on the owning user's window
+         *      
+         * TODO: For future versions check TYPE_ACCESSIBILITY_OVERLAY
+         */
         feedbackParams.type = WindowManager.LayoutParams.TYPE_PHONE | 
                 WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY;
                
-        // Whole screen is covered (including status bar)
-        feedbackParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE | 
+        /*
+         * Type of window. Whole screen is covered (including status bar)
+         * 
+         * FLAG_NOT_FOCUSABLE: this window won't ever get key input focus, so the user can not 
+         *      send key or other button events to it. It can use the full screen for its content 
+         *      and cover the input method if needed
+         *      
+         * FLAG_LAYOUT_IN_SCREEN: place the window within the entire screen, ignoring decorations 
+         *      around the border (such as the status bar)
+         *             
+         */
+        feedbackParams.flags = WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
                 WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
         
         feedbackParams.width = WindowManager.LayoutParams.MATCH_PARENT;

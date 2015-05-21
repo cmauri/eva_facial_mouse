d377a7743b59a7657749df15bca579074605303d
Cesar Mauri <cesar@crea>
2015-05-21 17:22:26 +0200

    improve some configuration options

--- eviacam_service/src/com/crea_si/eviacam/service/SettingsActivity.java
+++ eviacam_service/src/com/crea_si/eviacam/service/SettingsActivity.java
@@ -17,13 +17,37 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
- package com.crea_si.eviacam.service;
+package com.crea_si.eviacam.service;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceFragment;
 
+/**
+ * The settings activity
+ */
 public class SettingsActivity extends Activity {
+    
+    private static class ListPreferenceUpdate implements OnPreferenceChangeListener {
+
+        private final ListPreference mListPreference;
+        
+        public ListPreferenceUpdate(ListPreference lp) {
+            mListPreference = lp;
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            // Set the value as the new value
+            mListPreference.setValue(newValue.toString());                    
+            // Get the entry which corresponds to the current value and set as summary
+            preference.setSummary(mListPreference.getEntry());
+            return true;
+        }
+    }
     
     public static class SettingsFragment extends PreferenceFragment {
         @Override
@@ -32,6 +56,15 @@ public class SettingsActivity extends Activity {
 
             // Load the preferences from an XML resource
             addPreferencesFromResource(R.xml.preference_fragment);
+            
+            /**
+             * Listeners for list preference entries
+             */
+            ListPreference lp = (ListPreference) findPreference("docking_panel_edge");
+            lp.setOnPreferenceChangeListener(new ListPreferenceUpdate(lp));
+            
+            lp = (ListPreference) findPreference("time_without_detection");
+            lp.setOnPreferenceChangeListener(new ListPreferenceUpdate(lp));
         }
     }
     

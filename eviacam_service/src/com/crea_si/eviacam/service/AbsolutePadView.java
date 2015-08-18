a9bce4974994dd042429bd7d2422fb77fccccdaa
Cesar Mauri <cesar@crea>
2015-08-18 20:34:42 +0200

    partial abs pad view interface

---
+++ eviacam_service/src/com/crea_si/eviacam/service/AbsolutePadView.java
@@ -0,0 +1,180 @@
+/*
+ * Enable Viacam for Android, a camera based mouse emulator
+ *
+ * Copyright (C) 2015 Cesar Mauri Loba (CREA Software Systems)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.crea_si.eviacam.service;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PointF;
+import android.graphics.drawable.BitmapDrawable;
+import android.view.View;
+
+/**
+ * Layout for the absolute pad game controller
+ */
+
+public class AbsolutePadView extends View {
+    /*
+     * Main values for drawing game pad
+     */
+    private PointF mPadCenterNorm= new PointF(0.5f, 0.65f);
+    private float mOuterRadiusNorm= 0.2f;
+    private float mInnerRadiusNorm= 0.1f;
+    
+    // Cached paint box
+    private final Paint mPaintBox;
+    
+    /*
+     * Cached values that depend on canvas size
+     */
+    private float mCanvasWidth= 0;
+    private float mCanvasHeight= 0;
+    private float mPadCenterX= 0;
+    private float mPadCenterY= 0;
+    private float mOuterRadius= 0;
+    private float mInnerRadius= 0;
+    
+    // cached bitmaps and positions
+    private Bitmap[] mPadArrows= new Bitmap[8];
+    private Bitmap[] mPadArrowsPressed= new Bitmap[8];
+    private PointF[] mPadArrowsLocation= new PointF[8];
+    
+    public AbsolutePadView(Context c) {
+        super(c);
+
+        mPaintBox = new Paint();
+        setWillNotDraw(false);
+    }
+
+    @Override
+    public void onDraw(Canvas canvas){
+        super.onDraw(canvas);
+        
+        /*
+         * Check whether the canvas has been resized and update cached values if so 
+         */
+        if (mCanvasWidth!= canvas.getWidth() || mCanvasHeight!= canvas.getHeight()) {
+            mCanvasWidth= canvas.getWidth();
+            mCanvasHeight= canvas.getHeight();
+            mPadCenterX= mCanvasWidth * mPadCenterNorm.x;
+            mPadCenterY= mCanvasHeight * mPadCenterNorm.y;
+            final float canvasShortSize= (mCanvasWidth< mCanvasHeight? mCanvasWidth : mCanvasHeight);
+            mOuterRadius= canvasShortSize * mOuterRadiusNorm;
+            mInnerRadius= canvasShortSize * mInnerRadiusNorm;
+            
+            /*
+             * Read original bitmaps from resources
+             */
+            BitmapDrawable bd = (BitmapDrawable) getContext().getResources().
+                                    getDrawable(R.drawable.ic_pad_arrow_down, null);
+            Bitmap downArrowOrig= bd.getBitmap();
+            downArrowOrig.setDensity(Bitmap.DENSITY_NONE);
+            
+            bd = (BitmapDrawable) getContext().getResources().
+                    getDrawable(R.drawable.ic_pad_arrow_down_pressed, null);
+            Bitmap downArrowPressedOrig= bd.getBitmap();
+            downArrowPressedOrig.setDensity(Bitmap.DENSITY_NONE);
+            
+            /*
+             * Compute radius and size of the bitmaps (assume bitmap width > length) 
+             */
+            final float shortSide= (mOuterRadius - mInnerRadius) * 0.6f;
+            final float scaling = shortSide / (float) bd.getIntrinsicHeight();
+            final float longSide= scaling * bd.getIntrinsicWidth();
+            final float bmpCenterRadius = mInnerRadius + (mOuterRadius - mInnerRadius) / 2.0f;
+                
+            /*
+             * Scale to the desired size
+             */
+            Bitmap downArrowSized= Bitmap.createScaledBitmap(downArrowOrig, (int) longSide, (int) shortSide, true);
+            downArrowSized.setDensity(Bitmap.DENSITY_NONE);
+            
+            Bitmap downArrowPressedSized= Bitmap.createScaledBitmap(downArrowPressedOrig, (int) longSide, (int) shortSide, true);
+            downArrowPressedSized.setDensity(Bitmap.DENSITY_NONE);
+            
+            /*
+             * Initialize arrays (position 0 is arrow down)
+             */
+            mPadArrows[0]= downArrowSized;
+            mPadArrowsPressed[0]= downArrowPressedSized;
+            mPadArrowsLocation[0]= new PointF(
+                    mPadCenterX - (float) downArrowSized.getWidth()/2.0f,
+                    mPadCenterY + bmpCenterRadius - (float) downArrowSized.getHeight()/2.0f);
+    
+            double alpha= Math.PI / 2.0 + Math.PI / 4.0;
+            Matrix matrix = new Matrix();
+            int rotate= 45;
+            for (int i= 1; i< 8; i++) {
+                matrix.reset();
+                matrix.setRotate(rotate);
+                
+                Bitmap tb= Bitmap.createBitmap(downArrowSized, 0, 0, downArrowSized.getWidth(), downArrowSized.getHeight(), matrix, false);
+                tb.setDensity(Bitmap.DENSITY_NONE);
+                mPadArrows[i]= tb;
+                
+                tb= Bitmap.createBitmap(downArrowPressedSized, 0, 0, downArrowPressedSized.getWidth(), downArrowPressedSized.getHeight(), matrix, false);
+                tb.setDensity(Bitmap.DENSITY_NONE);
+                mPadArrowsPressed[i]= tb;
+                
+                mPadArrowsLocation[i]= new PointF(
+                        mPadCenterX + bmpCenterRadius * (float) Math.cos(alpha) - (float) tb.getWidth() / 2.0f,
+                        mPadCenterY + bmpCenterRadius * (float) Math.sin(alpha) - (float) tb.getHeight() / 2.0f);
+                
+                rotate+= 45;
+                alpha+= Math.PI / 4.0;
+            }
+        }
+
+        /*
+         * Draw sectors
+         */
+        mPaintBox.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(mPadCenterX, mPadCenterY, mOuterRadius, mPaintBox);
+        canvas.drawCircle(mPadCenterX, mPadCenterY, mInnerRadius, mPaintBox);
+
+        double alpha = Math.PI / 8.0;
+        for (int i= 0; i< 2; i++) {
+            final float cosAlpha = (float) Math.cos(alpha);
+            final float sinAlpha = (float) Math.sin(alpha);
+            
+            final float startX= mInnerRadius * cosAlpha;
+            final float stopX= mOuterRadius * cosAlpha;
+            final float startY= mInnerRadius * sinAlpha;
+            final float stopY= mOuterRadius * sinAlpha;
+            
+            canvas.drawLine(mPadCenterX + startX, mPadCenterY + startY, mPadCenterX + stopX, mPadCenterY + stopY, mPaintBox);
+            canvas.drawLine(mPadCenterX - startX, mPadCenterY + startY, mPadCenterX - stopX, mPadCenterY + stopY, mPaintBox);
+            canvas.drawLine(mPadCenterX + startX, mPadCenterY - startY, mPadCenterX + stopX, mPadCenterY - stopY, mPaintBox);
+            canvas.drawLine(mPadCenterX - startX, mPadCenterY - startY, mPadCenterX - stopX, mPadCenterY - stopY, mPaintBox);
+
+            alpha+= Math.PI / 4.0;
+        }
+
+        /*
+         * Draw arrows
+         */
+        for (int i= 0; i< 8; i++) {
+            // DEBUG //
+            canvas.drawBitmap(mPadArrowsPressed[i], mPadArrowsLocation[i].x, mPadArrowsLocation[i].y, mPaintBox);
+        }
+    }
+}
---
+++ eviacam_service/src/com/crea_si/eviacam/service/SlaveModeEngine.java
@@ -0,0 +1,98 @@
+/*
+ * Enable Viacam for Android, a camera based mouse emulator
+ *
+ * Copyright (C) 2015 Cesar Mauri Loba (CREA Software Systems)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.crea_si.eviacam.service;
+
+import android.content.Context;
+import android.graphics.PointF;
+import android.view.View;
+import android.view.accessibility.AccessibilityEvent;
+
+public class SlaveModeEngine {
+    
+    private AbsolutePadView mAbsolutePadView;
+    
+    // layer for drawing the pointer and the dwell click feedback
+    private PointerLayerView mPointerLayer;
+    
+    // object which provides the logic for the pointer motion and actions 
+    private PointerControl mPointerControl;
+    
+    public SlaveModeEngine(Context c, OverlayView ov) {
+        /*
+         * UI stuff 
+         */
+        mAbsolutePadView= new AbsolutePadView(c);
+        ov.addFullScreenLayer(mAbsolutePadView);
+
+        // pointer layer (should be the last one)
+        mPointerLayer= new PointerLayerView(c);
+        ov.addFullScreenLayer(mPointerLayer);
+
+        /*
+         * control stuff
+         */
+        mPointerControl= new PointerControl(c, mPointerLayer);
+    }
+   
+    public void cleanup() {
+        mPointerControl.cleanup();
+        mPointerControl= null;
+
+        mPointerLayer.cleanup();
+        mPointerLayer= null;
+    }
+   
+    public void pause() {
+        mPointerLayer.setVisibility(View.INVISIBLE);
+    }
+    
+    public void resume() {
+        mPointerControl.reset();
+        mPointerLayer.setVisibility(View.VISIBLE);
+    }    
+
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        // do nothing
+    } 
+
+    /*
+     * process incoming camera frame 
+     * 
+     * this method is called from a secondary thread 
+     */
+    public void processMotion(PointF motion) {
+        // update pointer location given face motion
+        mPointerControl.updateMotion(motion);
+        
+        // get new pointer location
+        PointF pointerLocation= mPointerControl.getPointerLocation();
+        
+        /*
+        Point pInt= new Point();
+        pInt.x= (int) pointerLocation.x;
+        pInt.y= (int) pointerLocation.y;
+        */
+        
+        mAbsolutePadView.postInvalidate();
+        
+        // update pointer position and click progress
+        mPointerLayer.updatePosition(pointerLocation);
+        mPointerLayer.postInvalidate();
+    }
+}

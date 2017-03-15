dd847d94ea90a11ac8612bba726f389179b4d230
Cesar Mauri <cesar@crea-si.com>
2017-03-15 20:22:55 +0100

    improve launcher activity and fix crash when no accessibility settings is available

--- eviacam/src/main/java/com/crea_si/eviacam/common/LauncherActivity.java
+++ eviacam/src/main/java/com/crea_si/eviacam/common/LauncherActivity.java
@@ -1,7 +1,7 @@
 /*
  * Enable Viacam for Android, a camera based mouse emulator
  *
- * Copyright (C) 2015 Cesar Mauri Loba (CREA Software Systems)
+ * Copyright (C) 2015-17 Cesar Mauri Loba (CREA Software Systems)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -21,11 +21,15 @@ package com.crea_si.eviacam.common;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 
-import com.crea_si.eviacam.common.Preferences;
 import com.crea_si.eviacam.R;
 
 /**
@@ -34,40 +38,80 @@ import com.crea_si.eviacam.R;
 
 public class LauncherActivity extends Activity {
 
-    private void openAccessibility() {
-        Intent intent = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
-        startActivityForResult(intent, 0);
+    /* Keep references to the dialogs to properly dismiss them. See:
+       http://stackoverflow.com/questions/2850573/activity-has-leaked-window-that-was-originally-added */
+    Dialog mHelpDialog;
+    Dialog mNoA11ySettingsDialog;
 
-        finish();
+    /**
+     * Display message for no accessibility settings available
+     */
+    private void noAccessibilitySettingsAlert() {
+        final Resources r= getResources();
+        mNoA11ySettingsDialog= new AlertDialog.Builder(this)
+            .setMessage(r.getText(R.string.no_accessibility_settings))
+            .setPositiveButton(r.getText(R.string.done),
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            dialogInterface.dismiss();
+                            finish();
+                        }
+                    })
+            .create();
+        mNoA11ySettingsDialog.setCancelable(false);
+        mNoA11ySettingsDialog.setCanceledOnTouchOutside(false);
+        mNoA11ySettingsDialog.show();
+    }
+
+    /**
+     * Open the accessibility settings screen
+     *
+     * @return true on success
+     */
+    private boolean openAccessibility() {
+        Intent intent = new Intent(android.provider.Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        try {
+            startActivity(intent, null);
+        }
+        catch(ActivityNotFoundException e) {
+            return false;
+        }
+        return true;
     }
 
     private void showLauncherHelp () {
-        final Resources r= getResources();
+        View checkBoxView = View.inflate(this, R.layout.launcher_help, null);
+        CheckBox checkBox = (CheckBox) checkBoxView.findViewById(R.id.checkbox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Preferences.get().setShowLauncherHelp(!isChecked);
+            }
+        });
 
-        new AlertDialog.Builder(this)
-        .setTitle(r.getText(R.string.app_name))
+        Resources r= getResources();
+        mHelpDialog= new AlertDialog.Builder(this)
         .setMessage(r.getText(R.string.how_to_run))
-        .setPositiveButton(r.getText(android.R.string.ok),
+        .setView(checkBoxView)
+        .setPositiveButton(r.getText(R.string.open_accessibility_settings),
                 new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        openAccessibility();
+                        dialogInterface.dismiss();
+                        if (!openAccessibility()) noAccessibilitySettingsAlert();
                     }
                 })
-        .setNeutralButton(r.getText(R.string.dont_remember_again),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Preferences.get().setShowLauncherHelp(false);
-                        openAccessibility();
-                    }
-                })
-        .show();
+        .create();
+        mHelpDialog.setCancelable(false);
+        mHelpDialog.setCanceledOnTouchOutside(false);
+        mHelpDialog.show();
     }
 
     @Override
-    protected void onStart () {
-        super.onStart();
+    protected void onResume() {
+        super.onResume();
 
         if (Preferences.initForA11yService(this) == null) return;
 
@@ -75,13 +119,26 @@ public class LauncherActivity extends Activity {
             showLauncherHelp();
         }
         else {
-            openAccessibility();
+            if (!openAccessibility()) noAccessibilitySettingsAlert();
         }
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
+
+        if (mHelpDialog!= null) {
+            mHelpDialog.dismiss();
+            mHelpDialog= null;
+        }
+
+        if (mNoA11ySettingsDialog!= null) {
+            mNoA11ySettingsDialog.dismiss();
+            mNoA11ySettingsDialog= null;
+        }
+
         Preferences.get().cleanup();
+
+        finish();
     }
 }
